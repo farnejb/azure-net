5ca95a35bea83d9c5d8bfca12fc77161a964915a
John Farnell <46055195+farnejb@users.noreply.github.com>
2023-01-10 11:57:45 -0500

    add provider

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+provider "azurerm" {
+  version = "~> 3.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "EastUS2"
+}
+
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
