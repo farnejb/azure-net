60718cb90c64c246f81def56ab3de355ed95477c
John Farnell <46055195+farnejb@users.noreply.github.com>
2023-01-11 13:30:14 -0500

    depends_on argument

--- main.tf
+++ main.tf
@@ -13,4 +13,8 @@ resource "azurerm_virtual_network" "example" {
   address_space       = ["10.0.0.0/16"]
   location            = "EastUS2" #azurerm_resource_group.example.location
   resource_group_name = "example-resources" #azurerm_resource_group.example.name
+  
+  depends_on = [
+    azurerm_resource_group.example
+  ]
 }
