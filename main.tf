75bea3a288383dc96420a913545ac558044d8f7e
John Farnell <46055195+farnejb@users.noreply.github.com>
2023-02-09 23:08:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_virtual_network" "example" {
   name                = "example-network-update-1"
   address_space       = ["10.0.0.0/16"]
   location            = "EastUS2" #azurerm_resource_group.example.location
-  resource_group_name = "example-resources" #azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.example.name
   
   tags = {
     environment = "dev"
